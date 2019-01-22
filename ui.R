f0b0a0d33069775e78903b4c308d117e6362f52a
Christian <cholland2408@gmail.com>
2019-01-22 13:46:25 +0100

    Added Piano tab

---
+++ ui.R
@@ -0,0 +1,23 @@
+# UI
+source("sub/global.R")
+ui = function(request) {
+  fluidPage(
+    #useShinyjs(),
+    #tags$head(includeScript("google-analytics.js")),
+    # theme = shinythemes::shinytheme("spacelab"),
+    navbarPage(
+      id = "menu", 
+      title = div(img(src="logo_saezlab.png", width="25", height="25"), "CKD-App"),
+      collapsible=T,
+      footer = column(12, align="center", "CKD-App 2019"),
+      source("sub/01_ui_welcome.R")$value,
+      source("sub/02_ui_experiments.R")$value,
+      source("sub/03_ui_deg.R")$value,
+      source("sub/04_ui_progeny.R")$value,
+      source("sub/05_ui_piano.R")$value,
+      source("sub/06_ui_dorothea.R")$value,
+      source("sub/07_ui_dvd.R")$value,
+      hr()
+      ) # close navbarPage
+    ) # close fluidPage
+}
