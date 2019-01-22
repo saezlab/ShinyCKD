efd3b6330fa5afa4866d6f95f5ff88020f0aad5e
Christian <cholland2408@gmail.com>
2019-01-22 13:47:34 +0100

    Added piano ui file

--- sub/05_ui_piano.R
+++ sub/05_ui_piano.R
@@ -4,13 +4,19 @@ tabPanel(
   sidebarPanel(
     h4("Pathway enrichment with Piano"),
     p(a("Piano", href = "https://bioconductor.org/packages/release/bioc/html/piano.html", targer="_blank"), "performs gene set analysis using various statistical methods, from different gene level statistics and combines the results of multiple runs."),
-    pickerInput(inputId = "select_disease_piano", label = "Select disease entity",
-                choices = sort(unique(limma_result$disease)), 
-                options = list(`actions-box` = TRUE)), 
-    pickerInput(inputId = "select_pathway_piano", label = "Select pathway",
-                choices = sort(unique(progeny_result$pathway))),
+    # pickerInput(inputId = "select_disease_piano", label = "Select disease entity",
+    #             choices = sort(unique(limma_result$disease)), 
+    #             options = list(`actions-box` = TRUE)), 
+    # pickerInput(inputId = "select_pathway_piano", label = "Select pathway",
+    #             choices = sort(unique(progeny_result$pathway))),
+    sliderInput(inputId = "adjust_fdr_piano", label = "Adjust FDR cutoff",
+                min=0.001, max=1, value = 0.05),
+    sliderInput(inputId = "select_common_diseases", label = "Pathways enriched in min 'n' diseases.",
+                min=1, max=9, value = 3, step = 1),
     downloadButton("download_piano", "Download enrichment scores")
   ),
   mainPanel(
+    plotOutput("piano_tile"),
+    DT::dataTableOutput("piano_df")
   )
 )
