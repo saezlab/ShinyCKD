b8bbd474e5dccbe8f73d6abb4da58149e5f70eda
Christian <cholland2408@gmail.com>
2019-01-22 13:46:12 +0100

    Added Piano tab

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ server = function(input, output, session) {
   source("sub/02_server_experiments.R", local=T)
   source("sub/03_server_deg.R", local=T)
   source("sub/04_server_progeny.R", local=T)
-  #source("sub/05_server_piano.R", local=T)
+  source("sub/05_server_piano.R", local=T)
   source("sub/06_server_dorothea.R", local=T)
   source("sub/07_server_dvd.R", local=T)
 }
