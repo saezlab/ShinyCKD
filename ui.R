28c8243998b37b796b865e3b332d398caaa371bf
Christian <cholland2408@gmail.com>
2019-01-23 11:24:02 +0100

    included google analytics

--- ui.R
+++ ui.R
@@ -2,14 +2,14 @@
 source("sub/global.R")
 ui = function(request) {
   fluidPage(
-    #useShinyjs(),
-    #tags$head(includeScript("google-analytics.js")),
-    # theme = shinythemes::shinytheme("spacelab"),
+    useShinyjs(),
+    tags$head(includeScript("google-analytics.js")),
     navbarPage(
       id = "menu", 
-      title = div(img(src="logo_saezlab.png", width="25", height="25"), "CKD-App"),
+      title = div(img(src="logo_saezlab.png", width="25", height="25"),
+                  "CKD-App"),
+      windowTitle = "CDK-App",
       collapsible=T,
-      footer = column(12, align="center", "CKD-App 2019"),
       source("sub/01_ui_welcome.R")$value,
       source("sub/02_ui_experiments.R")$value,
       source("sub/03_ui_deg.R")$value,
@@ -17,7 +17,7 @@ ui = function(request) {
       source("sub/05_ui_piano.R")$value,
       source("sub/06_ui_dorothea.R")$value,
       source("sub/07_ui_dvd.R")$value,
-      hr()
+      footer = column(12, align="center", "CKD-App 2019")
       ) # close navbarPage
     ) # close fluidPage
 }
